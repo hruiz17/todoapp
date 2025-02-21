3a2ca7db191fa9e31143b03ba75fd02b9a33a724
H <hruiz17@hotmail.com>
2025-02-20 20:17:55 -0500

    implementando estilos

---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-labs',
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  welcome = 'Bienvenido a mi primera aplicación con Angular';
+  tasks = [
+    'Instalar el Angular CLI',
+    'Crear proyecto',
+    'Crear componentes',
+    'Crear servicio',
+  ];
+}
